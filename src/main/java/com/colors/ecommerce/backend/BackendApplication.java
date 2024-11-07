00c809cc10e6b242cd5c964595f32fce09e434c1
furiac3lta <colummarcelo@gmail.com>
2024-11-06 23:55:30 -0300

    config cors

--- src/main/java/com/colors/ecommerce/backend/BackendApplication.java
+++ src/main/java/com/colors/ecommerce/backend/BackendApplication.java
@@ -3,6 +3,9 @@ package com.colors.ecommerce.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.mercadopago.MercadoPagoConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,6 +13,16 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		MercadoPagoConfig.setAccessToken("TEST-4941031144210285-021319-fda61d9e23aa19ab267ac409335d3db9-48037141");
 		SpringApplication.run(BackendApplication.class, args);
+
+
+	}
+	@Configuration
+	public class WebConfiguration implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedMethods("*");
+		}
 	}
 
 }

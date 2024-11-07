14c7b205504e4b83e7090a85685ddda682eed2cd
furiac3lta <colummarcelo@gmail.com>
2024-11-07 00:41:23 -0300

    Fix MapStruct configuration for UserMapper

--- src/main/java/com/colors/ecommerce/backend/BackendApplication.java
+++ src/main/java/com/colors/ecommerce/backend/BackendApplication.java
@@ -3,11 +3,13 @@ package com.colors.ecommerce.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.mercadopago.MercadoPagoConfig;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.colors.ecommerce"})
 public class BackendApplication {
 
 	public static void main(String[] args) {

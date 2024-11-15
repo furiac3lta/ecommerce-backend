081b74d83a943d7e1930f1f900539162236c45fa
furiac3lta <colummarcelo@gmail.com>
2024-11-15 02:04:56 -0300

    ajustando expired time token1

--- src/main/java/com/colors/ecommerce/backend/infrastucture/jwt/Constants.java
+++ src/main/java/com/colors/ecommerce/backend/infrastucture/jwt/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String HEADER_AUTHORIZATION = "Authorization";
     public static final String TOKEN_BEARER_PREFIX = "Bearer ";
     public static final String SUPER_SECRET_KEY = "fS2UAxl7vRy0XTLfb89RxPEZAjWa6LG2sfKhp_hSz9sAxr9WYsaV4jJ0QL-NUcCT1m1JfL8";
-    public static final long TOKEN_EXPIRATION_TIME = 10000; //1500000;
+    public static final long TOKEN_EXPIRATION_TIME = 600000; //600000;
 
     public static Key getSignedKey(String secretKey){
         byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
